05fc4bc64fed1fe3be8b3588df6b1681e8bcbd76
bazag <bazag@DESKTOP-OVMJGDL>
2019-10-31 19:10:04 +0100

    Activitat 3, execució de fibonacci amb fils

---
+++ UF2/src/Activitat3.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class Activitat3 extends RecursiveTask<Long> {
+
+	long numero;
+
+	public Activitat3(long numero){
+		this.numero=numero;
+	}    
+
+	@Override
+	protected Long compute() {
+		double calcul = java.lang.Math.cos(54879854);
+		if(numero <= 1) return numero;
+		Activitat3 fib1 = new Activitat3(numero-1);
+		//fib1.fork();
+		Activitat3 fib2 = new Activitat3(numero-2);
+		fib2.fork();
+		return fib1.compute()+ fib2.join();
+	}
+
+	public static void main(String[] args){
+		// Cogemos el tiempo inicial
+		long inicio = System.currentTimeMillis();
+		ForkJoinPool pool = new ForkJoinPool();
+		long resultado = pool.invoke(new Activitat3(40));   
+		// Cogemos el tiempo final
+        long fin = System.currentTimeMillis();
+     // Lo pasamos a segundos
+        double tiempo = (double) ((fin - inicio)/1000);
+     // Mostramos tanto el tiempo de ejecución como el resultado
+        System.out.println("Segundos: " + tiempo + ", resultado: " + resultado);
+	}
+}
